f00949901daad14e4b1653060f0609765086a32c
shaileshshiva <shaileshshiva28@gmail.com>
2023-01-15 18:03:48 +0530

    third  commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
 
-   static class A{
+   private static class A{
 
         String meth(){
             return "Invoking method from class A";
         }
     }
 
-    static class B extends A{
+    private class B extends A{
 
         @Override
         String meth() {
