e71228c3e1713091ca687b36ca160bc6461879ba
shaileshshiva <shaileshshiva28@gmail.com>
2023-01-15 23:20:10 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[]args){
 
         B b = new B();
-        System.out.println(b.meth());
+//        System.out.println(b.meth());
+        B b1 = new B();
+//        System.out.println(b1.meth());
+
       }
 
     public static class A{
